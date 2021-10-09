85798cf430750856c560fcd5505ea1e08f26ecd8
haun <haun@126.com>
2021-10-10 02:27:22 +0800

    add max.cpp

---
+++ 03_cpp_常用函数/08_max.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int a = 10, b = 20;
+    int ret = max(a, b);
+    cout << ret << endl;
+    return -1;
+}
