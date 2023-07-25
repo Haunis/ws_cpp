3715de534f2a503ecfd91dc2f6961baa9c094f8a
haun <haun@126.com>
2023-07-25 23:46:47 +0800

    update 07_3_string

--- 02_cpp_基础/07_3_string.cpp
+++ 02_cpp_基础/07_3_string.cpp
@@ -3,26 +3,26 @@
  * isalnum(char c): 是否是0或者字母
  * islower(char c): 是否是小写字母
  * isupper(char c): 是否是大写字母
- * 
+ *
  * 数据类型转换
- * 1.string转char*: 
+ * 1.string转char*:
  *      1.string.data()
  *          const char *char_p = ss.data();
- *          char *char_p = (char *)ss.data(); 
+ *          char *char_p = (char *)ss.data();
  *		2.string.c_str()
  *		3.str.copy(char[],0,5)
- *		参考: https://www.cnblogs.com/wuruofeng/p/10412559.html 
+ *		参考: https://www.cnblogs.com/wuruofeng/p/10412559.html
  *
  * 2.string转int：
  *      1.std::stoi("12");
- *      2.stringstream(string ss) >> ret  
+ *      2.stringstream(string ss) >> ret
  * 		  stringstream是一个类,将string类型的变量转换成其他如int ,float类型的变量
- * 
+ *
  * 3.int转string：
  *      string ss = std::to_string(12)
  * 4.char转string：
- *      
- * 
+ *
+ *
  */
 
 #include <iostream>
@@ -39,11 +39,11 @@ int main(int argc, char *argv[])
     cout << s.length() << endl;
 
     cout << "-------------2. s[0]------------" << endl;
-    cout << typeid(s[s[0]]).name() << endl; //c, char类型
-    cout << s[4] << endl;                   //某个位置的字符
+    cout << typeid(s[s[0]]).name() << endl; // c, char类型
+    cout << s[4] << endl;                   // 某个位置的字符
 
     cout << "-------------3.isalnum()------------" << endl;
-    cout << isalnum(',') << endl; //0; 是否是0或者字母
+    cout << isalnum(',') << endl; // 0; 是否是0或者字母
 
     cout << "-------------4.islower()------------" << endl;
     cout << islower('a') << endl; // true, 非0; 是否是小写
@@ -53,13 +53,14 @@ int main(int argc, char *argv[])
 
     cout << "-------------5.1 string转char*-------------" << endl;
     string ss = "hello";
-    //	const char *char_p = ss.data();
-    char *char_p = (char *)ss.data();                                  //转换后就是一个普通指针
-    printf("char_p=%s, sizeof(char_p)=%lu\n", char_p, sizeof(char_p)); //size是8,指针
+    //	const char *char_p = ss.data();//常量指针，里面内容不可修改
+    char *char_p = (char *)ss.data(); // 转换后就是一个普通指针
+    // const char *char_p = ss.c_str();//常量指针，里面内容不可修改
+    printf("char_p=%s, sizeof(char_p)=%lu\n", char_p, sizeof(char_p)); // size是8,指针
 
-    *char_p = 'x';                 //不是常量指针，可以为里面的内容重新赋值
-    printf("char_p=%s\n", char_p); //xello
-    cout << "ss=" << ss << endl;   //xello
+    *char_p = 'x';                 // 不是常量指针，可以为里面的内容重新赋值
+    printf("char_p=%s\n", char_p); // xello
+    cout << "ss=" << ss << endl;   // xello
 
     cout << "\n-----------5.2 string转int-------------------" << endl;
     string ss12 = "3";
@@ -74,16 +75,16 @@ int main(int argc, char *argv[])
     // string str = 9 + '0'; //error;单个数字转换成string
     string str;
     str = 9 + '0';
-    cout << str << endl; //9
+    cout << str << endl; // 9
 
     cout << "\n-----------5.4 char转string-------------------" << endl;
     char c = '1';
     string ret = "";
 
-    ret = c; //这个也ok??
+    ret = c; // 这个也ok??
     ret += c;
-    cout << ret << endl; //11
+    cout << ret << endl; // 11
 
-    cout << string(4, c) << endl; //1111
+    cout << string(4, c) << endl; // 1111
     return -1;
 }
